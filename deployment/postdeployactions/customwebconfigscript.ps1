05de36f8cbecc1f1061ecf12eded875fa88a523f
040305 <040305@USHOM00NB040305>
2018-06-28 18:18:58 -0500

    1.30

--- deployment/postdeployactions/customwebconfigscript.ps1
+++ deployment/postdeployactions/customwebconfigscript.ps1
@@ -28,12 +28,12 @@ $notificationEmailFrom.value = $env:APPSETTING_key14;
 
 $root = $doc.get_DocumentElement();
 $pllConString = $root.connectionStrings.add | ? {$_.name -eq 'PLLConnection'}
-$pllnewCon = $pllConString.connectionString.Replace('Server=;Initial Catalog=;Persist Security Info=False;User ID=;Password=;','Server='+$srvsql+';Initial Catalog=pwadb;Persist Security Info=False;User ID='+$usrsql+';Password='+$psdsql+';');
+$pllnewCon = $pllConString.connectionString.Replace('Server=;Initial Catalog=;Persist Security Info=False;User ID=;Password=;','Server='+$srvsql+';Initial Catalog=cpssodb;Persist Security Info=False;User ID='+$usrsql+';Password='+$psdsql+';');
 $pllConString.connectionString = $pllnewCon
 
 
 $PrivateLabelConString = $root.connectionStrings.add | ? {$_.name -eq 'PrivateLabelLiteDataEntities'}
-$newPrivateLabelConString = $PrivateLabelConString.connectionString.Replace('data source=;initial catalog=plltest;User ID=;Password=','data source='+$srvsql+';Initial Catalog=pwadb;User ID='+$usrsql+';Password='+$psdsql);
+$newPrivateLabelConString = $PrivateLabelConString.connectionString.Replace('data source=;initial catalog=plltest;User ID=;Password=','data source='+$srvsql+';Initial Catalog=cpssodb;User ID='+$usrsql+';Password='+$psdsql);
 $PrivateLabelConString.connectionString = $newPrivateLabelConString
 
 $root."system.net".mailSettings.smtp.network.userName = $env:APPSETTING_key14
